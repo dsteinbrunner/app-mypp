521a8c4ed08e29a812412129fed9abf1167907ff
Jan Henning Thorsen <jhthorsen@cpan.org>
2010-04-21 23:10:36 +0200

    add t/my-test-project

---
+++ t/my-test-project/lib/My/Test/Project.pm
@@ -0,0 +1,24 @@
+package \ # hide from pause?
+ My::Test::Project;
+
+=head1 NAME
+
+My::Test::Project - can do stuff
+
+=head1 VERSION
+
+0.01
+
+=cut
+
+use strict;
+use warnings;
+use IPC::Open2;
+
+=head1 AUTHOR
+
+See L<App::Mypp>.
+
+=cut
+
+1;
