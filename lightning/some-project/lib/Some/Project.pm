1f6b3730a58a7dc9e14aede56f03a81b064b648b
Jan Henning Thorsen <jhthorsen@cpan.org>
2010-08-03 07:25:38 +0200

    add some-project

---
+++ lightning/some-project/lib/Some/Project.pm
@@ -0,0 +1,34 @@
+package Some::Project;
+
+=head1 NAME
+
+Some::Project - can do stuff
+
+=head1 VERSION
+
+0.00
+
+=head1 DESCRIPTION
+
+=head1 SYNOPSIS
+
+=cut
+
+use Moose;
+use Math::BigInt;
+
+=head1 ATTRIBUTES
+
+=head1 METHODS
+
+=head1 BUGS
+
+=head1 COPYRIGHT & LICENSE
+
+=head1 AUTHOR
+
+Jan Henning Thorsen C<< jhthorsen at cpan.org >>
+
+=cut
+
+1;
