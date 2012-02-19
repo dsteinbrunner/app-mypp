34c3b9fec0c7a0b385b42010b71ead81d0f8a088
Jan Henning Thorsen <jhthorsen@cpan.org>
2012-02-19 18:28:43 +0100

    0.12        * Rewrote mypp. Changed api and application switches        * Improved rollback on "build"        * Use Applify to set up application

--- lib/App/Mypp.pm
+++ lib/App/Mypp.pm
@@ -6,7 +6,7 @@ App::Mypp - Maintain Your Perl Project
 
 =head1 VERSION
 
-0.11
+0.12
 
 =head1 DESCRIPTION
 
@@ -103,7 +103,7 @@ use Cwd;
 use File::Basename;
 use File::Find;
 
-our $VERSION = eval '0.11';
+our $VERSION = eval '0.12';
 our $SILENT = $ENV{'MYPP_SILENT'} || $ENV{'SILENT'} || 0;
 our $PAUSE_FILENAME = $ENV{'HOME'} .'/.pause';
 our $VERSION_RE = qr/\d+ \. [\d_]+/x;
