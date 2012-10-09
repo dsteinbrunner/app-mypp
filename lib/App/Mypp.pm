91ed8cf0b191eea527a734d7c92f6984f0ead1fa
Jan Henning Thorsen <jhthorsen@cpan.org>
2012-10-09 20:22:59 +0200

    0.1301        * Fix RT79493: Cannot run END{} block unless .git/ was found

--- lib/App/Mypp.pm
+++ lib/App/Mypp.pm
@@ -6,7 +6,7 @@ App::Mypp - Maintain Your Perl Project
 
 =head1 VERSION
 
-0.13
+0.1301
 
 =head1 DESCRIPTION
 
@@ -103,7 +103,7 @@ use Cwd;
 use File::Basename;
 use File::Find;
 
-our $VERSION = eval '0.13';
+our $VERSION = eval '0.1301';
 our $SILENT = $ENV{MYPP_SILENT} || $ENV{SILENT} || 0;
 our $PAUSE_FILENAME = $ENV{HOME} .'/.pause';
 our $VERSION_RE = qr/\d+ \. [\d_]+/x;
