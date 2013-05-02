feb9684d1937bd5b119bcb6b7291aa260852e5db
Jan Henning Thorsen <jhthorsen@cpan.org>
2013-05-02 11:06:50 +0200

    Released version 0.15
           * Change commit message

--- lib/App/Mypp.pm
+++ lib/App/Mypp.pm
@@ -6,7 +6,7 @@ App::Mypp - Maintain Your Perl Project
 
 =head1 VERSION
 
-0.14
+0.15
 
 =head1 DESCRIPTION
 
@@ -103,7 +103,7 @@ use Cwd;
 use File::Basename;
 use File::Find;
 
-our $VERSION = eval '0.14';
+our $VERSION = eval '0.15';
 our $SILENT = $ENV{MYPP_SILENT} || $ENV{SILENT} || 0;
 our $PAUSE_FILENAME = $ENV{HOME} .'/.pause';
 our $VERSION_RE = qr/\d+ \. [\d_]+/x;
